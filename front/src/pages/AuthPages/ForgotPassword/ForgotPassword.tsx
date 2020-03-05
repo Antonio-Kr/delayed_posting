bb090104570c5a3d2d7bbf370a2237dbf1cae4e8
zsmax <max.ziuzin@gmail.com>
2020-03-05 12:23:08 +0200

    go back function

--- front/src/pages/AuthPages/ForgotPassword/ForgotPassword.tsx
+++ front/src/pages/AuthPages/ForgotPassword/ForgotPassword.tsx
@@ -1,37 +1,40 @@
 import React from 'react'
 import classes from './../style.module.scss'
-import { Button, Form, Input } from 'antd'
-import { layout, tailLayout } from '../LoginPage'
+import {Button, Form, Input} from 'antd'
+import {layout, tailLayout} from '../LoginPage'
 
-export default function(props: any) {
-  return (
-    <div className={classes.sign}>
-      <div className={classes.sign__form}>
-        <h2 className={classes.sign__form_title}>Forgot Your Password?</h2>
-        <Form
-          {...layout}
-          name="signUpForm"
-          initialValues={{ remember: true }}
-          // onFinish={onFinish}
-          // onFinishFailed={onFinishFailed}
-        >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: 'Please input your e-mail!' }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item {...tailLayout}>
-            <div className={classes.socials_justify}>
-              <Button size={'large'} type="primary" htmlType="submit">
-                Reset
-              </Button>
-              <Button size={'large'}>Back</Button>
+export default function (props: any) {
+    return (
+        <div className={classes.sign}>
+            <div className={classes.sign__form}>
+                <h2 className={classes.sign__form_title}>Forgot Your Password?</h2>
+                <Form
+                    {...layout}
+                    name="signUpForm"
+                    initialValues={{remember: true}}
+                    // onFinish={onFinish}
+                    // onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{required: true, message: 'Please input your e-mail!'}]}
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item {...tailLayout}>
+                        <div className={classes.socials_justify}>
+                            <Button size={'large'} type="primary" htmlType="submit">
+                                Reset
+                            </Button>
+                            <Button
+                                onClick={props.history.goBack}
+                                size={'large'}
+                            >Back</Button>
+                        </div>
+                    </Form.Item>
+                </Form>
             </div>
-          </Form.Item>
-        </Form>
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
