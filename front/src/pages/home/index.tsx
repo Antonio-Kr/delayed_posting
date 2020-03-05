daed9d97336496b98f6fc280be8d16e4eeada6aa
zsmax <max.ziuzin@gmail.com>
2020-03-05 18:27:20 +0200

    unauthorised redirect

--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -3,12 +3,29 @@ import classes from './style.module.scss'
 import Nav from "./modules/nav";
 import Main from './modules/main';
 import Agenda from "./modules/agenda";
+import {StateType} from "typesafe-actions";
+import {connect} from "react-redux";
+import {Redirect} from 'react-router-dom';
 
-export default function Home() {
 
-    return <h1 className={classes.home}>
-        <Nav/>
-        <Main/>
-        <Agenda/>
-    </h1>
+function Home(props: StateType<any>) {
+
+    if (props.user === null) return <Redirect to={'/login'}/>
+
+    return (
+        <h1 className={classes.home}>
+            <Nav/>
+            <Main/>
+            <Agenda/>
+        </h1>
+    )
 }
+
+const mapStateToProps = (state: StateType<any>) => {
+
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(Home)
