bff473aa5c13f330fc46e1648f06ce8df23e4516
zsmax <max.ziuzin@gmail.com>
2020-03-06 14:22:34 +0200

    test url

--- front/src/API/api.ts
+++ front/src/API/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import {Simulate} from "react-dom/test-utils";
 
-const baseURL = 'http://localhost:3000';
+// const baseURL = 'http://localhost:3000';
+const baseURL = 'https://jsonplaceholder.typicode.com/users/';
 
 const instance = axios.create({
     baseURL,
@@ -12,9 +14,24 @@ const instance = axios.create({
     },
 });
 
+interface IUserData {
+    name: string
+    email: string
+    password: string
+}
+
 export const UserApi = {
-    getAuthTag() {
-        instance.get('')
+    getUser(id: number) {
+        instance.get(`/${id}`)
+            .then(response => response.data)
+    },
+    registerUser(userData: IUserData) {
+        instance.post('', {
+            ...userData
+        })
+            .then(({data}) => data)
+            .then(console.log)
+            .catch(console.log)
     },
     getLoginInfo() {
         instance.get('')
