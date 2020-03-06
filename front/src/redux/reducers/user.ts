ebcd1fe62be7815382e1264531822c00d5dec2d1
zsmax <max.ziuzin@gmail.com>
2020-03-06 14:23:06 +0200

    small changes

--- front/src/redux/reducers/user.ts
+++ front/src/redux/reducers/user.ts
@@ -1,5 +1,5 @@
 import {handleActions} from 'redux-actions';
-import {IAddress, IUser} from '../../types';
+import {IUser} from '../../types';
 import {UserActions} from '../actions';
 
 const initialState = null;
@@ -8,7 +8,7 @@ const initialState = null;
 // fake  initial state
 
 // const initialState = {
-//     email: 'string',
+// email: 'string',
 //     status: 'string',
 //     avatar: 'string',
 //     lastName: 'string',
@@ -22,13 +22,17 @@ const initialState = null;
 //     },
 //     profession: 'string',
 //     phone: 'string',
-//     roles: ['1','2','3'],
+//     roles: ['1', '2', '3'],
 //
 // }
 
 export const UserReducer = handleActions<IUser | null, IUser>({
-    [UserActions.Type.FETCHING_USER]: (state) => state,
-    [UserActions.Type.GET_USER]: (state, action) => action.payload,
-    [UserActions.Type.SET_USER]: (state, action) => action.payload,
+    [UserActions.Type.SET_USER]: (state, action) => ({
+        state,
+        ...action.payload
+    }), // registration?
 
 }, initialState);
+
+
+
