372a405f3537008d7ad69df811ebeecf0d0b4b12
zsmax <max.ziuzin@gmail.com>
2020-03-06 16:12:51 +0200

    redirect to login after sign Up

--- front/src/pages/AuthPages/SignUpPage/SignUpPage.tsx
+++ front/src/pages/AuthPages/SignUpPage/SignUpPage.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import classes from '../style.module.scss'
 import SignUpLoginPage, {signType} from '../modules/signUp/signUp'
 import {connect} from 'react-redux'
 import {SignUpForm} from "./SignUpForm";
 import {UserActions} from "../../../redux/actions";
+import {IUser} from '../../../types';
+import {StateType} from "typesafe-actions";
+import {Redirect} from "react-router-dom";
 
 function SignUpPage(props: any) {
     const signName: signType = {
@@ -11,19 +14,25 @@ function SignUpPage(props: any) {
         linkName: 'Log In',
         path: '/login',
     }
-
+    if (props.user !== null) return <Redirect to={'/login'}/>
     return (
         <div className={classes.sign}>
             <SignUpLoginPage {...signName} />
             <div className={classes.sign__form}>
                 <h2 className={classes.sign__form_title}>Log in</h2>
-                <SignUpForm />
+                <SignUpForm setUser={props.setUser}/>
             </div>
         </div>
     )
 }
 
-const mapDispatchToProps = (dispatch: any) => {
+const mapStateToProps = (state: StateType<any>) => {
+    return {
+        user: state.user.user
+    }
 }
 
-export default connect(null, mapDispatchToProps)(SignUpPage)
+const mapDispatchToProps = {
+    setUser: (user: IUser) => (UserActions.setUser(user))
+}
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpPage)
