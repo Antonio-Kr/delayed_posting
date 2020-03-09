e5fc0b4a0919c48257ed4650fd193ecb58e7567b
zsmax <maxim.maniak@gmail.com>
2020-03-09 18:45:58 +0200

    HOME btn

--- front/src/pages/AuthPages/modules/sentRegigter/index.tsx
+++ front/src/pages/AuthPages/modules/sentRegigter/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {CheckOutlined} from '@ant-design/icons';
 import {Button} from "antd";
 import classes from './style.module.scss';
+import {NavLink} from "react-router-dom";
 
 
 export const SentRegister = (props: any) => {
@@ -12,6 +13,7 @@ export const SentRegister = (props: any) => {
                 onClick={props.history.goBack}
                 size={'large'}
             >Back</Button>
+            <NavLink to={'/'}><Button type={"link"}>HOME</Button></NavLink>
         </div>
     )
 }
