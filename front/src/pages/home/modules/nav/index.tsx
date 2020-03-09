5a6c36d4e9b70446b78663053a4fc5b8590aa0ac
zsmax <maxim.maniak@gmail.com>
2020-03-09 18:47:19 +0200

    small fixes

--- front/src/pages/home/modules/nav/index.tsx
+++ front/src/pages/home/modules/nav/index.tsx
@@ -6,10 +6,10 @@ import {Button} from 'antd'
 export default function Nav() {
     return (
         <div className={classes.nav}>
-            <NavLink className={classes.nav__link} to={'/:posts'}>Posts</NavLink>
-            <NavLink className={classes.nav__link} to={'/:calendar'}>Calendar</NavLink>
-            <NavLink className={classes.nav__link} to={'/:statistics'}>Statistics</NavLink>
-            <NavLink className={classes.nav__link} to={'/:archive'}>Archive</NavLink>
+            <NavLink className={classes.nav__link} to={'/posts'}>Posts</NavLink>
+            <NavLink className={classes.nav__link} to={'/calendar'}>Calendar</NavLink>
+            <NavLink className={classes.nav__link} to={'/statistics'}>Statistics</NavLink>
+            <NavLink className={classes.nav__link} to={'/archive'}>Archive</NavLink>
         </div>
     )
 }
